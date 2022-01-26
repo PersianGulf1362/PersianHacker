327d6b6b18d79070f35829c7eeae45e252a411a5
PersianGulf1362 <59501263+PersianGulf1362@users.noreply.github.com>
2022-01-26 12:50:38 +0100

    Discord

---
+++ commands/help.js
@@ -0,0 +1,43 @@
+const { MessageEmbed } = require('discord.js')
+
+module.exports = {
+    info: {
+        name: "help",
+        description: "برای نمایش تمام دستورات",
+        usage: "[command]",
+        aliases: ["commands", "help me", "pls help"]
+    },
+
+    run: async function(client, message, args){
+        var allcmds = "";
+
+        client.commands.forEach(cmd => {
+            let cmdinfo = cmd.info
+            allcmds+="``"+client.config.prefix+cmdinfo.name+" "+cmdinfo.usage+"`` ~ "+cmdinfo.description+"\n"
+        })
+
+        let embed = new MessageEmbed()
+        .setAuthor("Commands of "+client.user.username, "https://raw.githubusercontent.com/SudhanPlayz/Discord-MusicBot/master/assets/Music.gif")
+        .setColor("BLUE")
+        .setDescription(allcmds)
+        .setFooter(`برای دریافت اطلاعات هر فرمان می توانید انجام دهید ${client.config.prefix}help [فرمان] | Made by Schatz#8611 | Mohammad`)
+
+        if(!args[0])return message.channel.send(embed)
+        else {
+            let cmd = args[0]
+            let command = client.commands.get(cmd)
+            if(!command)command = client.commands.find(x => x.info.aliases.includes(cmd))
+            if(!command)return message.channel.send("Unknown Command")
+            let commandinfo = new MessageEmbed()
+            .setTitle("Command: "+command.info.name+" info")
+            .setColor("YELLOW")
+            .setDescription(`
+Name: ${command.info.name}
+Description: ${command.info.description}
+Usage: \`\`${client.config.prefix}${command.info.name} ${command.info.usage}\`\`
+Aliases: ${command.info.aliases.join(", ")}
+`)
+            message.channel.send(commandinfo)
+        }
+    }
+}
